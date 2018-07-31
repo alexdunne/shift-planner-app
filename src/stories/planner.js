0428fa4b84534d33658aa2d9495077c89671c95f
Alex Dunne <hi@alexdunne.net>
2018-07-31 23:50:31 +0100

    Fixed the planner story - was passing an object rather the the field value

--- src/stories/planner.js
+++ src/stories/planner.js
@@ -1,8 +1,43 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { withState } from "@dump247/storybook-state";
 
+import { Container, Row, Col } from "../components/Grid";
 import Planner from "../components/Planner";
 
-storiesOf("Planner", module).add("Planner", () => (
-  <Planner month={7} year={2018} />
-));
+const now = new Date();
+
+storiesOf("Planner", module).add(
+  "Planner",
+  withState({ month: now.getMonth() + 1, year: now.getFullYear() })(
+    ({ store }) => (
+      <Container>
+        <Row>
+          <Col>
+            <input
+              type="number"
+              value={store.state.month}
+              onChange={event =>
+                store.set({ month: parseInt(event.target.value, 10) })
+              }
+            />
+          </Col>
+          <Col>
+            <input
+              type="number"
+              value={store.state.year}
+              onChange={event =>
+                store.set({ year: parseInt(event.target.value, 10) })
+              }
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Planner month={store.state.month} year={store.state.year} />
+          </Col>
+        </Row>
+      </Container>
+    )
+  )
+);
