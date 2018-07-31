667e8a540e1db2d8f66cc9052dcb2458b1b94581
Alex Dunne <hi@alexdunne.net>
2018-07-31 22:00:52 +0100

    Added storybook state addon

--- src/stories/app-header.js
+++ src/stories/app-header.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { withState } from "@dump247/storybook-state";
 
 import AppHeader from "../components/AppHeader";
 
-storiesOf("AppHeader", module)
-  .add("locked", () => <AppHeader locked={true} />)
-  .add("unlocked", () => <AppHeader locked={false} />);
+storiesOf("AppHeader", module).add(
+  "Toggle lock",
+  withState({ locked: true })(({ store }) => (
+    <AppHeader
+      {...store.state}
+      onToggleLocked={() => store.set({ locked: !store.state.locked })}
+    />
+  ))
+);
