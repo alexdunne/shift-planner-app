824411816dec0c038c83f15ab2575dee57c7c6cb
Alex Dunne <hi@alexdunne.net>
2018-08-12 21:45:53 +0100

    Removed loading shift type from localstorage manually

--- src/index.js
+++ src/index.js
@@ -6,44 +6,41 @@ import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { configureStore } from "./store";
-import { SHIFT_TYPES_STORAGE_KEY } from "./utils/constants";
 
 import "./index.css";
 import "./flex.css";
 import "./spacing.css";
 
-const shiftTypes = localStorage.getItem(SHIFT_TYPES_STORAGE_KEY)
-  ? JSON.parse(localStorage.getItem(SHIFT_TYPES_STORAGE_KEY))
-  : {
-      byId: {
-        "0d4bdc27-fcb0-469d-b398-af68d6da02fa": {
-          displayName: "Day shift",
-          reference: "day_shift",
-          color: "#687878"
-        },
-        "19b6879d-40a8-40dc-9cb4-a35f6f2959a8": {
-          displayName: "Night shift",
-          reference: "night_shift",
-          color: "#DD435B"
-        },
-        "99dead20-a9e1-4c2b-be3d-763e6e5395c8": {
-          displayName: "Holiday",
-          reference: "holiday",
-          color: "#2196F3"
-        },
-        "60789850-c73f-4f70-ac37-3aece381e96f": {
-          displayName: "Study day",
-          reference: "study_day",
-          color: "#FF9800"
-        }
-      },
-      allIds: [
-        "0d4bdc27-fcb0-469d-b398-af68d6da02fa",
-        "19b6879d-40a8-40dc-9cb4-a35f6f2959a8",
-        "99dead20-a9e1-4c2b-be3d-763e6e5395c8",
-        "60789850-c73f-4f70-ac37-3aece381e96f"
-      ]
-    };
+const shiftTypes = {
+  byId: {
+    "0d4bdc27-fcb0-469d-b398-af68d6da02fa": {
+      displayName: "Day shift",
+      reference: "day_shift",
+      color: "#687878"
+    },
+    "19b6879d-40a8-40dc-9cb4-a35f6f2959a8": {
+      displayName: "Night shift",
+      reference: "night_shift",
+      color: "#DD435B"
+    },
+    "99dead20-a9e1-4c2b-be3d-763e6e5395c8": {
+      displayName: "Holiday",
+      reference: "holiday",
+      color: "#2196F3"
+    },
+    "60789850-c73f-4f70-ac37-3aece381e96f": {
+      displayName: "Study day",
+      reference: "study_day",
+      color: "#FF9800"
+    }
+  },
+  allIds: [
+    "0d4bdc27-fcb0-469d-b398-af68d6da02fa",
+    "19b6879d-40a8-40dc-9cb4-a35f6f2959a8",
+    "99dead20-a9e1-4c2b-be3d-763e6e5395c8",
+    "60789850-c73f-4f70-ac37-3aece381e96f"
+  ]
+};
 
 const { store, persistor } = configureStore({ shiftTypes });
 
